54af4a0e4602ea03e20266dff8d4ec3275c98c36
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-06-05 09:05:22 -0500

    Create function for insertOne

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,38 @@
 var connection = require("./connection.js");
 
+function questionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+
 var orm = {
-    selectAll: function(tableInput) {
+    selectAll: function(tableInput, cb) {
         var queryString = "SELECT * FROM ??";
         connection.query(queryString, [tableInput], function(err, res) {
             if (err) throw (err);
-            console.log(res)
+            cb(res)
+        });
+    },
+    insertOne: function(table, column, value) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += column.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += questionMarks(value.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, res) {
+            if (err) throw err;
+            cb(res);
         });
     }
 }
