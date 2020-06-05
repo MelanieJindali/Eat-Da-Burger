aabe3a3b5789a59e95a9a786b2b78e15926d098c
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-06-05 14:32:43 -0500

    Add GET routes for "/"

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+// Redirect to index pathway
+router.get("/", function(req, res) {
+    res.redirect("/index");
+});
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        // Handlebars object
+        var hbsObj = { burgers: data };
+        console.log(hbsObj);
+        res.render("index", hbsObj);
+    });
+});
