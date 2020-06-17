7754d7f60e2f2102692b380d908fe5eab41e63dc
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-06-17 12:56:39 -0500

    Add funcationality to burger buttons

---
+++ public/assets/js/burger.js
@@ -0,0 +1,28 @@
+$(function() {
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+
+    var newBurg = {
+      name: $("#newBurg").val().trim()
+    };
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurg,
+    }).then(function() {
+      console.log("created new burger");
+      location.reload(); // Refresh Page
+    });
+  });
+
+  $(".devour").on("click", function(event) {
+    var id = $(this).data("id");
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+    }).then(function() {
+      console.log("Burger has been devoured!");
+      location.reload();
+    });
+  });
+});
